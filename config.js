71ca4272334c5c14e1e12962862778e5c80d6734
hoan.lengoc <ngochoanhhcc3@gmail.com>
2024-10-19 18:08:04 +0700

    1

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Thùy Linh';
+const giftUrl = 'https://ngochoanhhcc3.github.io/20_10/';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng Thùy Linh xinh';
+const contentCard = 'Chúc Thùy Linh 20/10 tràn ngập niềm vui và những nụ cười, KPI vượt chỉ tiêu, mọi điều thuân lợi';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
